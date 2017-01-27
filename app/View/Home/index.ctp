c0c0a4dd45942ff2183dce9e600cb29b213f125f
Oscar Valecillos <oscarvalecillosg@gmail.com>
2017-01-26 21:48:16 -0400

    arreglos cemon

--- app/View/Home/index.ctp
+++ app/View/Home/index.ctp
@@ -27,15 +27,15 @@
 					<option value="2006">2006</option>
 					<option value="2007">2007</option>
 					<option value="2008">2008</option>
-					<option value="2001">2009</option>
-					<option value="2001">2010</option>
-					<option value="2001">2011</option>
-					<option value="2001">2012</option>
-					<option value="2001">2013</option>
-					<option value="2001">2014</option>
-					<option value="2001">2015</option>
-					<option value="2001">2016</option>
-					<option value="2001">2017</option>
+					<option value="2009">2009</option>
+					<option value="2010">2010</option>
+					<option value="2011">2011</option>
+					<option value="2012">2012</option>
+					<option value="2013">2013</option>
+					<option value="2014">2014</option>
+					<option value="2015">2015</option>
+					<option value="2016">2016</option>
+					<option value="2017">2017</option>
 				</select>
 		  </div>
 		</form>
--- app/webroot/js/cemon.js
+++ app/webroot/js/cemon.js
@@ -1,40 +1,65 @@
 $(document).ready(function(){
 
-    $("#monitor").click(function(){
+    $("#monitor").unbind('click').click(function(){
 
       $(".panel").removeClass("hidden");
-      var url1 = "http://localhost/tarea_1_CE/";
+      //var url1 = window.location.hostname+"/";
       var url2 = "/disponibilidad/"+$("#mes").val()+"-"+$("#anio").val();
 
-      var servidor = url1+"servidor"+url2;
-      var smdb = url1+"smbd"+url2;
-      var aplicacion = url1+"aplicacion"+url2;
-      var internet = url1+"internet"+url2;
-      var router = url1+"router"+url2;
+      var servidor ="servidor"+url2;
+      var smdb = "smbd"+url2;
+      var aplicacion = "aplicacion"+url2;
+      var internet = "internet"+url2;
+      var router = "router"+url2;
       var disponTotal = 1;
+      var error=0;
 
+      $("#resultados").empty();
 
       $.ajax({ dataType: "json", async: false, url: servidor,  success: function(result){
-        $("#resultados").append("<tr><td class = \"text-center\">"+result["nombre"]+"</td><td class = \"text-center\">"+result["disponibilidad"]+"</td>");
-        disponTotal = disponTotal * result["disponibilidad"];
+        if(result["error"]!="BAD REQUEST"){
+          $("#resultados").append("<tr><td class = \"text-center\">"+result["nombre"]+"</td><td class = \"text-center\">"+result["disponibilidad"]+"</td>");
+          disponTotal = disponTotal * result["disponibilidad"];
+          error=1;
+        }
       }});
       $.ajax({ dataType: "json", async: false, url: smdb,  success: function(result){
-        $("#resultados").append("<tr><td class = \"text-center\">"+result["nombre"]+"</td><td class = \"text-center\">"+result["disponibilidad"]+"</td>");
-          disponTotal = disponTotal * result["disponibilidad"];
+        if(!result["error"]!="BAD REQUEST"){
+          $("#resultados").append("<tr><td class = \"text-center\">"+result["nombre"]+"</td><td class = \"text-center\">"+result["disponibilidad"]+"</td>");
+            disponTotal = disponTotal * result["disponibilidad"];
+            error=1;
+        }
       }});
       $.ajax({ dataType: "json", async: false, url: aplicacion,  success: function(result){
-        $("#resultados").append("<tr><td class = \"text-center\">"+result["nombre"]+"</td><td class = \"text-center\">"+result["disponibilidad"]+"</td>");
-          disponTotal = disponTotal * result["disponibilidad"];
+        if(!result["error"]!="BAD REQUEST"){        
+          $("#resultados").append("<tr><td class = \"text-center\">"+result["nombre"]+"</td><td class = \"text-center\">"+result["disponibilidad"]+"</td>");
+            disponTotal = disponTotal * result["disponibilidad"];
+            error=1;
+        }
       }});
       $.ajax({ dataType: "json", async: false, url: internet,  success: function(result){
-        $("#resultados").append("<tr><td class = \"text-center\">"+result["nombre"]+"</td><td class = \"text-center\">"+result["disponibilidad"]+"</td>");
-          disponTotal = disponTotal * result["disponibilidad"];
+        if(!result["error"]!="BAD REQUEST"){        
+          $("#resultados").append("<tr><td class = \"text-center\">"+result["nombre"]+"</td><td class = \"text-center\">"+result["disponibilidad"]+"</td>");
+            disponTotal = disponTotal * result["disponibilidad"];
+            error=1;
+        }
       }});
       $.ajax({ dataType: "json", async: false, url: router,  success: function(result){
-        $("#resultados").append("<tr><td class = \"text-center\">"+result["nombre"]+"</td><td class = \"text-center\">"+result["disponibilidad"]+"</td>");
-          disponTotal = disponTotal * result["disponibilidad"];
+        if(!result["error"]!="BAD REQUEST"){        
+          $("#resultados").append("<tr><td class = \"text-center\">"+result["nombre"]+"</td><td class = \"text-center\">"+result["disponibilidad"]+"</td>");
+            disponTotal = disponTotal * result["disponibilidad"];
+            error=1;
+        }
       }});
 
-      $(".panel-footer").append(disponTotal);
+      console.log(error);
+
+      if(error==1){
+        $(".panel-footer").empty().append(disponTotal);
+        error=0;
+      }else{
+        $(".panel-footer").empty().append('<h3>Ha ocurrido un error con la fecha, verif&iacute;quela</h3>');        
+      }
+
     });
 });
